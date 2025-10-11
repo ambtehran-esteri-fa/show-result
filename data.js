f9ae083f81f5e33945146eb9fab88b6182d4b486
ambtehran-esteri-fa <ambtehran-esteri-fa@tuta.io>
2025-10-11 07:03:09 +0330

    Create data.js

---
+++ data.js
@@ -0,0 +1,40 @@
+// data.js
+const userData = [
+  {
+    username: "Mashal Saidy",
+    password: "IT659349731",
+    ceuNumber: "659349733",
+    name: "Mashal",
+    lastname: "Saidy",
+    nationality: "Afghan",
+    passportNumber: "p909090",
+    nationalIDNumber: "p909090",
+    gender: "Female",
+    links: {
+      // جایگزین کن با مسیر واقعی PDF در repo یا raw.githubusercontent
+      finalResult: "pdf/mashal_result.pdf",
+      uploadPassport: "https://example.com/mashal-passport",
+      uploadDocuments: "https://example.com/mashal-docs",
+      unhcrLetter: "https://example.com/mashal-unhcr",
+      uploadFinger: "https://example.com/mashal-finger"
+    }
+  },
+  {
+    username: "MASHAL SAIDY",
+    password: "411914534",
+    ceuNumber: "CEU411914534",
+    name: "MASHAL",
+    lastname: "SAIDY",
+    nationality: "AFGHAN",
+    passportNumber: "",
+    nationalIDNumber: "00000",
+    gender: "female",
+    links: {
+      finalResult: "pdf/mashal2_result.pdf",
+      uploadPassport: "https://example.com/mashal2-passport",
+      uploadDocuments: "https://example.com/mashal2-docs",
+      unhcrLetter: "https://example.com/mashal2-unhcr",
+      uploadFinger: "https://example.com/mashal2-finger"
+    }
+  }
+];
